1b7d91c40776695d06b4af2ed1a13464ac5f1356
Mike Macgirvin <mike@macgirvin.com>
2010-09-08 20:47:22 -0700

    update global directory on change of profile photo

--- mod/profile_photo.php
+++ mod/profile_photo.php
@@ -76,6 +76,15 @@ function profile_photo_post(&$a) {
 				dbesc(datetime_convert())
 			);
 
+			// Update global directory in background
+			$php_path = ((strlen($a->config['php_path'])) ? $a->config['php_path'] : 'php');
+			$url = $_SESSION['my_url'];
+			if($url && strlen(get_config('system','directory_submit_url')))
+				proc_close(proc_open("\"$php_path\" \"include/directory.php\" \"$url\" &",
+					array(),$foo));
+
+
+
 		}
 		goaway($a->get_baseurl() . '/profiles');
 		return; // NOTREACHED
