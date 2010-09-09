a5b3b29286f011c332856e586532cb60421af3d1
Mike Macgirvin <mike@macgirvin.com>
2010-09-09 01:41:31 -0700

    missing brace

--- include/poller.php
+++ include/poller.php
@@ -108,7 +108,7 @@
 		}
 
 		$final_dfrn_id = substr($final_dfrn_id, 0, strpos($final_dfrn_id, '.'));
-		if($final_dfrn_id != $idtosend)
+		if($final_dfrn_id != $idtosend) {
 			// did not decode properly - cannot trust this site 
 			continue;
 		}
